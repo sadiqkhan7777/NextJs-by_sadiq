8b789ce0291f2ca4160198c0a1b228b0b3a71004
sadiq khan <saiqkhan7777@gmail.com>
2024-10-13 11:44:30 +0500

    Delete ASSIGNMENTS/5-Portfolio directory

--- ASSIGNMENTS/5-Portfolio/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['i.imgur.com'], // Allow images from imgur
-    },
-  };
-  
-  export default nextConfig;
-  
