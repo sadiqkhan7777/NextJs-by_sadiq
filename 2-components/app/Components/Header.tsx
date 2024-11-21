afe470e02b311836a64602c66b0e80680d6db989
sadiq khan <saiqkhan7777@gmail.com>
2024-11-21 07:04:07 +0500

    Add files via upload

---
+++ 2-components/app/Components/Header.tsx
@@ -0,0 +1,8 @@
+
+function Header (){
+    return(
+        <h1>this is Header </h1>
+    )
+}
+
+export default Header 
---
+++ 2-components/app/page.tsx
@@ -0,0 +1,17 @@
+import About from "./Components/AboutUs";
+import Header from "./Components/Header";
+export default function Home() {
+    return(
+        //<> </> fragments
+        //div 
+    <div>
+     <h1>Thid is a Homepage!</h1>
+    <Header/>
+    <About/>
+     </div>
+    );
+}
+
+
+//  create 2 components which names its Header.tsx * About.tsx
+// and import in homepage
